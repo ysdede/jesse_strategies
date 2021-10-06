b17756bef618efb6b1895f782f06be4c2e831e6f
ysdede <yunusseyhandede@gmail.com>
2021-10-06 17:24:45 +0300

    Add logs and genetics

---
+++ playground/optimization/ott2butKAMA2-400days/routes.py
@@ -0,0 +1,38 @@
+routes = [
+    ('Binance', 'BTC-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'ETH-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'ADA-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'XRP-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'BNB-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'LINK-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'LTC-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'EOS-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'ETC-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'OMG-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    ('Binance', 'TRX-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'THETA-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'VET-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'XLM-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'WAVES-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'XMR-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'ICX-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'NEO-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'ZEC-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'IOTA-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'QTUM-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'DASH-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'ONT-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'NULS-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'BTT-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'HOT-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'ZIL-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'ZRX-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'BAT-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'CELR-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'NANO-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),
+    # ('Binance', 'ENJ-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),  # 2019-04-19
+    # ('Binance', 'MATIC-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),  # 2019-04-27
+    # ('Binance', 'FTM-USDT', '5m', 'Ott2butKAMA', '(╯°□°)╯︵ ┻━┻'),  # 2019-06-22
+]
+
+extra_candles = []
