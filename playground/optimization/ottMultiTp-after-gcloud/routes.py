7f870e37280634fdad18a3f5f568fa8aa8bd31fa
ysdede <yunusseyhandede@gmail.com>
2021-10-06 17:29:02 +0300

    Add genetic logs, pickles

---
+++ playground/optimization/ottMultiTp-after-gcloud/routes.py
@@ -0,0 +1,9 @@
+routes = [
+    # ('Binance Futures', 'ANCHOR!', '5m', 'ottMultiTp', 'Z,McYkh'),
+    ('Binance', 'ETH-USDT', '5m', 'ottMultiTp', 'Z,McYkh'),
+    ('Binance', 'FTM-USDT', '5m', 'ottMultiTp', 'Z,McYkh'),
+    ('Binance', 'SUSHI-USDT', '5m', 'ottMultiTp', 'Z,McYkh'),
+    ('Binance', 'VET-USDT', '5m', 'ottMultiTp', 'Z,McYkh'),
+]
+
+extra_candles = []
