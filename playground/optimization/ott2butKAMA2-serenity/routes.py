03a44e62111d6b3f9685a579f9cfcb3462d723e4
ysdede <yunusseyhandede@gmail.com>
2021-10-06 17:27:32 +0300

    Add more files

---
+++ playground/optimization/ott2butKAMA2-serenity/routes.py
@@ -0,0 +1,15 @@
+routes = [
+    # ('Binance Futures', 'ANCHOR!', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'FTM-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'BLZ-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'ETH-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'ZIL-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'TRX-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'LINK-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'VET-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'SOL-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+    ('Binance Futures', 'DOGE-USDT', '5m', 'Ott2butKAMAposs', 'c@Rr9.'),
+]
+
+extra_candles = []
+
