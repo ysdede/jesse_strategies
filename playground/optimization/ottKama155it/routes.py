cc260b20202d771f0f195c5039b964bc05beafb2
ysdede <yunusseyhandede@gmail.com>
2021-10-06 17:28:14 +0300

    .gitignore

---
+++ playground/optimization/ottKama155it/routes.py
@@ -0,0 +1,34 @@
+routes = [
+    ('Binance Futures', 'ZRX-USDT', '5m', 'ottKama155it', 'r0W:I'),  # 4.06
+    ('Binance Futures', 'CELR-USDT', '5m', 'ottKama155it', 'r0W:I'),  # 0.61
+    ('Binance Futures', 'ETH-USDT', '5m', 'ottKama155it', 'r0W:I'),  # 2.12
+    # ('Binance Futures', 'SAND-USDT', '5m', 'ottKama155it', 'r0W:I'),#  -2.79
+    ('Binance Futures', 'AAVE-USDT', '5m', 'ottKama155it', 'r0W:I'),  # 3.13
+    # ('Binance Futures', 'UNI-USDT', '5m', 'ottKama155it', 'r0W:I'),  # -1
+    # ('Binance Futures', 'CVC-USDT', '5m', 'ottKama155it', 'r0W:I'),#  -3
+    ('Binance Futures', 'SOL-USDT', '5m', 'ottKama155it', 'r0W:I'),  # -1.12
+    #
+    ('Binance Futures', 'CRV-USDT', '5m', 'ottKama155it', 'r0W:I'),  # 0.66
+
+    #
+    # ('Binance Futures', 'BAL-USDT', '5m', 'ottKama155it', 'r0W:I'), -6
+    # ('Binance Futures', 'FIL-USDT', '5m', 'ottKama155it', 'r0W:I'), -1.87
+    # ('Binance Futures', 'DASH-USDT', '5m', 'ottKama155it', 'r0W:I'),    0.26
+    # ('Binance Futures', 'BNB-USDT', '5m', 'ottKama155it', 'r0W:I'), 0.43
+]
+
+extra_candles = []
+
+# ZRX-USDT
+# CELR-USDT
+# ETH-USDT
+# SAND-USDT
+# AAVE-USDT
+# UNI-USDT
+# CVC-USDT
+# SOL-USDT
+# BAL-USDT
+# CRV-USDT
+# FIL-USDT
+# DASH-USDT
+# BNB-USDT
