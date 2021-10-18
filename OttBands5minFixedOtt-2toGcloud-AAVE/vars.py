f2691b9ed34961c74809762b0c4d934f588ee7a8
Yunus S. Dede <yunusseyhandede@gmail.com>
2021-10-18 16:25:50 +0300

    Update vars.py

--- OttBands5minFixedOtt-2toGcloud-AAVE/vars.py
+++ OttBands5minFixedOtt-2toGcloud-AAVE/vars.py
@@ -1,35 +1,3 @@
-# tp1 = tp2 = tp3 = 0
-# total = 10
-# start = 1
-#
-# print('START')
-#
-# print('tps = \n')
-#
-#
-# def generate(start, total):
-#     count = 0
-#     for i in range(start, total):
-#         tp1 = i
-#         for j in range(start, total):
-#             tp2 = j
-#             for k in range(start, total):
-#                 tp3 = k
-#                 for l in range(start, total):
-#                     tp4 = l
-#                     for m in range(start, total):
-#                         tp5 = m
-#
-#                         if i + j + k + l + m == total:
-#                             count += 1
-#                             # print(f'i: {i}, j: {j}, k: {k}')
-#                             print(f'({i}, {j}, {k}, {l}, {m}),')
-#     return count
-#
-#
-# c = generate(start, total)
-# print(c)
-
 tp_qtys = (  # len = 126
     (1, 1, 1, 1, 6),
     (1, 1, 1, 2, 5),
