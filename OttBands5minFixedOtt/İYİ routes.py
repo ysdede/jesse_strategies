5d4b122d71da0ff7e8299b5c9154e7690c3f4a73
ysdede <yunusseyhandede@gmail.com>
2021-10-18 15:59:06 +0300

    New strat

---
+++ OttBands5minFixedOtt/İYİ routes.py
@@ -0,0 +1,16 @@
+routes = [
+    # ('Binance Futures', 'TRX-USDT', '5m', 'OttBands5minFixed', 'BVWWv'),
+    # ('Binance Futures', '1INCH-USDT', '5m', 'OttBands5minFixed', 'WVsWv'),
+    # ('Binance Futures', 'AAVE-USDT', '5m', 'OttBands5minFixed', '=ptvt'), # +
+    # ('Binance Futures', 'BCH-USDT', '5m', 'OttBands5minFixed', 'dl*.M'),
+    # ('Binance Futures', 'MATIC-USDT', '5m', 'OttBands5minFixed', 'ui*5<'), # +
+    # ('Binance Futures', 'CRV-USDT', '5m', 'OttBands5minFixed', 'ePjWv'),
+    # ('Binance Futures', 'SUSHI-USDT', '5m', 'OttBands5minFixed', '3OWXC'),
+    # ('Binance Futures', 'LINK-USDT', '5m', 'OttBands5minFixed', 'BVWWv'),  # +
+    # ('Binance Futures', 'CELR-USDT', '5m', 'OttBands5minFixed', '?VWWg'),
+    # ('Binance Futures', 'FTM-USDT', '5m', 'OttBands5minFixed', 'eEsWv'),  # ++
+    # ('Binance Futures', 'SOL-USDT', '5m', 'OttBands5minFixed', '=ptvt'),
+    ('Binance Futures', 'BTC-USDT', '5m', 'OttBands5minFixed', '-dW6n'),  # +
+]
+
+extra_candles = []
