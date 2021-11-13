cfaf287c5e5d4792f734bbd2f5670c3749960d7d
ysdede <yunusseyhandede@gmail.com>
2021-11-13 14:44:20 +0300

    Update

---
+++ Ott2butKAMA2-400Ds/ott2butKAMA2-400daysRe3/jessetkdata/dnafiles/2019-2021-11-50.py
@@ -0,0 +1,102 @@
+dnas = [
+    ['V5sC-', 33, 105, 32.88, 54, 11, 4.03,
+        {'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2899, 'risk_reward': 542, 'chop_bandwidth': 1327}],
+    ['FbQfv', 46, 30, 20.68, 16, 6, 3.54, {
+        'ott_ma_len': 43, 'ott_percent': 5171, 'stop_loss': 2038, 'risk_reward': 985, 'chop_bandwidth': 3175}],
+    ['[.bN+', 37, 107, 22.13, 37, 16, 1.45,
+        {'ott_ma_len': 70, 'ott_percent': 1880, 'stop_loss': 2468, 'risk_reward': 681, 'chop_bandwidth': 1276}],
+    ['j6<s)', 35, 85, 41.12, 36, 11, 7.61, {
+        'ott_ma_len': 89, 'ott_percent': 2386, 'stop_loss': 1506, 'risk_reward': 1149, 'chop_bandwidth': 1225}],
+    ['t(MQB', 37, 82, 44.25, 60, 10, 18.14, {
+        'ott_ma_len': 101, 'ott_percent': 1500, 'stop_loss': 1937, 'risk_reward': 719, 'chop_bandwidth': 1858}],
+    ['c5SmB', 37, 74, 77.67, 42, 7, 6.58, {
+        'ott_ma_len': 80, 'ott_percent': 2323, 'stop_loss': 2089, 'risk_reward': 1073, 'chop_bandwidth': 1858}],
+    ['U5Sm;', 40, 93, 49.84, 50, 12, 10.54, {
+        'ott_ma_len': 62, 'ott_percent': 2323, 'stop_loss': 2089, 'risk_reward': 1073, 'chop_bandwidth': 1681}],
+    ['V5Zs2', 37, 103, 63.07, 50, 12, -0.39,
+        {'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2266, 'risk_reward': 1149, 'chop_bandwidth': 1453}],
+    ['e6uf6', 38, 72, 67.58, 50, 8, 16.74, {
+        'ott_ma_len': 82, 'ott_percent': 2386, 'stop_loss': 2949, 'risk_reward': 985, 'chop_bandwidth': 1554}],
+    ['s\\^fn', 43, 30, 38.31, 25, 4, 0.72, {
+        'ott_ma_len': 100, 'ott_percent': 4791, 'stop_loss': 2367, 'risk_reward': 985, 'chop_bandwidth': 2972}],
+    ['j(cN:', 36, 94, 16.14, 8, 12, -11.24,
+        {'ott_ma_len': 89, 'ott_percent': 1500, 'stop_loss': 2494, 'risk_reward': 681, 'chop_bandwidth': 1656}],
+    ['j8cG:', 38, 70, 48.65, 33, 6, -5.24,
+        {'ott_ma_len': 89, 'ott_percent': 2513, 'stop_loss': 2494, 'risk_reward': 592, 'chop_bandwidth': 1656}],
+    ['j9cf)', 37, 77, 55.04, 25, 8, 1.57, {
+        'ott_ma_len': 89, 'ott_percent': 2576, 'stop_loss': 2494, 'risk_reward': 985, 'chop_bandwidth': 1225}],
+    ['V5[m-', 35, 107, 44.34, 50, 12, -0.41,
+        {'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2291, 'risk_reward': 1073, 'chop_bandwidth': 1327}],
+    ['V5js5', 40, 100, 79.39, 45, 11, -5.52,
+        {'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2671, 'risk_reward': 1149, 'chop_bandwidth': 1529}],
+    ['U5Qm6', 36, 100, 29.0, 50, 12, 9.56, {
+        'ott_ma_len': 62, 'ott_percent': 2323, 'stop_loss': 2038, 'risk_reward': 1073, 'chop_bandwidth': 1554}],
+    ['f6cu)', 36, 76, 50.89, 36, 11, 30.48, {
+        'ott_ma_len': 83, 'ott_percent': 2386, 'stop_loss': 2494, 'risk_reward': 1175, 'chop_bandwidth': 1225}],
+    ['j-cf)', 37, 95, 34.21, 50, 10, 5.99,
+        {'ott_ma_len': 89, 'ott_percent': 1816, 'stop_loss': 2494, 'risk_reward': 985, 'chop_bandwidth': 1225}],
+    ['p.Y@8', 48, 83, 66.66, 50, 8, 0.66,
+        {'ott_ma_len': 96, 'ott_percent': 1880, 'stop_loss': 2241, 'risk_reward': 504, 'chop_bandwidth': 1605}],
+    ['V5sm)', 34, 105, 77.03, 45, 11, -5.63,
+        {'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2899, 'risk_reward': 1073, 'chop_bandwidth': 1225}],
+    ['k7DkB', 34, 69, 38.14, 50, 6, 4.96, {
+        'ott_ma_len': 90, 'ott_percent': 2449, 'stop_loss': 1709, 'risk_reward': 1048, 'chop_bandwidth': 1858}],
+    ['f1pLn', 39, 33, 13.82, 66, 3, 8.63, {
+        'ott_ma_len': 83, 'ott_percent': 2070, 'stop_loss': 2823, 'risk_reward': 656, 'chop_bandwidth': 2972}],
+    ['[V^Xn', 37, 37, 59.95, 66, 3, 8.38, {
+        'ott_ma_len': 70, 'ott_percent': 4411, 'stop_loss': 2367, 'risk_reward': 808, 'chop_bandwidth': 2972}],
+    ['[<kI;', 35, 77, 35.39, 22, 9, 0.53, {
+        'ott_ma_len': 70, 'ott_percent': 2766, 'stop_loss': 2696, 'risk_reward': 618, 'chop_bandwidth': 1681}],
+    ['f5^m1', 39, 82, 45.35, 33, 6, -3.79,
+        {'ott_ma_len': 83, 'ott_percent': 2323, 'stop_loss': 2367, 'risk_reward': 1073, 'chop_bandwidth': 1428}],
+    ['j,cf)', 36, 96, 33.68, 50, 12, 0.46, {
+        'ott_ma_len': 89, 'ott_percent': 1753, 'stop_loss': 2494, 'risk_reward': 985, 'chop_bandwidth': 1225}],
+    ['t5Oj-', 39, 79, 55.32, 33, 9, 13.06,
+        {'ott_ma_len': 101, 'ott_percent': 2323, 'stop_loss': 1987, 'risk_reward': 1035, 'chop_bandwidth': 1327}],
+    ['V5sm@', 39, 82, 51.5, 40, 10, -0.03,
+        {'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2899, 'risk_reward': 1073, 'chop_bandwidth': 1808}],
+    ['s\\Klg', 39, 33, 32.57, 0, 4, -6.39,
+        {'ott_ma_len': 100, 'ott_percent': 4791, 'stop_loss': 1886, 'risk_reward': 1061, 'chop_bandwidth': 2795}],
+    ['eYufn', 46, 26, 48.08, 20, 5, 10.27, {
+        'ott_ma_len': 82, 'ott_percent': 4601, 'stop_loss': 2949, 'risk_reward': 985, 'chop_bandwidth': 2972}],
+    ['kYTLn', 48, 25, 40.93, 20, 5, 1.71, {
+        'ott_ma_len': 90, 'ott_percent': 4601, 'stop_loss': 2114, 'risk_reward': 656, 'chop_bandwidth': 2972}],
+    ['eYufg', 45, 31, 65.05, 20, 5, 10.27, {
+        'ott_ma_len': 82, 'ott_percent': 4601, 'stop_loss': 2949, 'risk_reward': 985, 'chop_bandwidth': 2795}],
+    ['eYufu', 47, 21, 25.87, 0, 2, -1.21,
+        {'ott_ma_len': 82, 'ott_percent': 4601, 'stop_loss': 2949, 'risk_reward': 985, 'chop_bandwidth': 3149}],
+    ['n\\^fn', 40, 27, 48.3, 0, 4, -5.43,
+        {'ott_ma_len': 94, 'ott_percent': 4791, 'stop_loss': 2367, 'risk_reward': 985, 'chop_bandwidth': 2972}],
+    ['U\\Sot', 47, 34, 31.08, 0, 3, -1.33,
+        {'ott_ma_len': 62, 'ott_percent': 4791, 'stop_loss': 2089, 'risk_reward': 1099, 'chop_bandwidth': 3124}],
+    ['[Vbfp', 40, 35, 44.52, 66, 3, 10.94, {
+        'ott_ma_len': 70, 'ott_percent': 4411, 'stop_loss': 2468, 'risk_reward': 985, 'chop_bandwidth': 3023}],
+    ['[VoYo', 36, 36, 38.13, 66, 3, 10.26, {
+        'ott_ma_len': 70, 'ott_percent': 4411, 'stop_loss': 2797, 'risk_reward': 820, 'chop_bandwidth': 2997}],
+    ['_;T^]', 37, 51, 36.44, 60, 5, 12.44, {
+        'ott_ma_len': 75, 'ott_percent': 2703, 'stop_loss': 2114, 'risk_reward': 884, 'chop_bandwidth': 2542}],
+    ['V5Zs?', 39, 88, 39.81, 40, 10, 0.29, {
+        'ott_ma_len': 63, 'ott_percent': 2323, 'stop_loss': 2266, 'risk_reward': 1149, 'chop_bandwidth': 1782}],
+    ['U/d*P', 45, 95, 18.32, 43, 16, 3.46,
+        {'ott_ma_len': 62, 'ott_percent': 1943, 'stop_loss': 2519, 'risk_reward': 225, 'chop_bandwidth': 2213}],
+    ['t5Xs>', 40, 67, 51.8, 28, 7, 2.82, {
+        'ott_ma_len': 101, 'ott_percent': 2323, 'stop_loss': 2215, 'risk_reward': 1149, 'chop_bandwidth': 1757}],
+    ['e<uf6', 28, 76, 33.61, 28, 7, 8.61, {
+        'ott_ma_len': 82, 'ott_percent': 2766, 'stop_loss': 2949, 'risk_reward': 985, 'chop_bandwidth': 1554}],
+    ['t5?m-', 39, 81, 45.07, 30, 10, 9.11,
+        {'ott_ma_len': 101, 'ott_percent': 2323, 'stop_loss': 1582, 'risk_reward': 1073, 'chop_bandwidth': 1327}],
+    ['c5pm6', 35, 84, 64.39, 25, 8, -6.08,
+        {'ott_ma_len': 80, 'ott_percent': 2323, 'stop_loss': 2823, 'risk_reward': 1073, 'chop_bandwidth': 1554}],
+    ['j5cs3', 36, 72, 50.5, 25, 8, -1.27, {'ott_ma_len': 89, 'ott_percent': 2323,
+                                           'stop_loss': 2494, 'risk_reward': 1149, 'chop_bandwidth': 1478}],
+    ['p.mII', 49, 67, 41.73, 57, 7, 5.2, {
+        'ott_ma_len': 96, 'ott_percent': 1880, 'stop_loss': 2747, 'risk_reward': 618, 'chop_bandwidth': 2035}],
+    ['t5O]-', 39, 79, 44.27, 33, 9, 11.39,
+        {'ott_ma_len': 101, 'ott_percent': 2323, 'stop_loss': 1987, 'risk_reward': 871, 'chop_bandwidth': 1327}],
+    ['eYuff', 42, 33, 58.26, 20, 5, 10.27, {
+        'ott_ma_len': 82, 'ott_percent': 4601, 'stop_loss': 2949, 'risk_reward': 985, 'chop_bandwidth': 2770}],
+    ['[`^fn', 40, 30, 30.04, 25, 4, -3.45,
+        {'ott_ma_len': 70, 'ott_percent': 5044, 'stop_loss': 2367, 'risk_reward': 985, 'chop_bandwidth': 2972}],
+    ['[V^wo', 37, 37, 51.74, 66, 3, 12.96, {
+        'ott_ma_len': 70, 'ott_percent': 4411, 'stop_loss': 2367, 'risk_reward': 1200, 'chop_bandwidth': 2997}],
+]
---
+++ Ott2butKAMA2-400Ds/ott2butKAMA2-400daysRe3/routes.py
@@ -0,0 +1,5 @@
+routes = [
+    ('Binance Futures', 'ETH-USDT', '5m', 'Ott2butKAMARe3', 't(MQB')
+]
+
+extra_candles = []
