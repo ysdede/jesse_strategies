370e288a0f400de8a44e3f0130cc2da8e196de20
ysdede <yunusseyhandede@gmail.com>
2021-11-13 14:39:26 +0300

    Update

---
+++ Ott2butKAMA2-400Ds/ott2butKAMA2-400days/jessetkdata/dnafiles/ETH-USDT 2021-09-26 2021-10-24.py
@@ -0,0 +1,7 @@
+dnas = [
+['L2jH:L', 42, 285, 121.24, 33, 51, 4.08, {'ott_len': 35, 'ott_percent': 189, 'stop_loss': 342, 'risk_reward': 38, 'chop_rsi_len': 19, 'chop_bandwidth': 165}],
+['c?Qt=K', 39, 198, 132.41, 38, 34, 13.05, {'ott_len': 57, 'ott_percent': 304, 'stop_loss': 232, 'risk_reward': 77, 'chop_rsi_len': 21, 'chop_bandwidth': 161}],
+['C3<j+W', 40, 400, 116.7, 35, 68, -1.19, {'ott_len': 27, 'ott_percent': 197, 'stop_loss': 139, 'risk_reward': 68, 'chop_rsi_len': 5, 'chop_bandwidth': 212}],
+[':2d95Z', 39, 368, 115.8, 32, 65, 0.35, {'ott_len': 19, 'ott_percent': 189, 'stop_loss': 316, 'risk_reward': 25, 'chop_rsi_len': 14, 'chop_bandwidth': 225}],
+[':2c95Z', 39, 368, 108.4, 32, 65, 1.93, {'ott_len': 19, 'ott_percent': 189, 'stop_loss': 311, 'risk_reward': 25, 'chop_rsi_len': 14, 'chop_bandwidth': 225}],
+]
---
+++ Ott2butKAMA2-400Ds/ott2butKAMA2-400days/jessetkdata/dnafiles/Ott2butKAMAdnas.py
@@ -0,0 +1,6 @@
+dnas = [
+['j4LV)a', 50, 73, 54.73, 66, 12, 15.43, {'ott_len': 63, 'ott_percent': 206, 'stop_loss': 209, 'risk_reward': 51, 'chop_rsi_len': 3, 'chop_bandwidth': 255}],
+['c3QlI[', 51, 85, 85.8, 45, 20, 14.52, {'ott_len': 57, 'ott_percent': 197, 'stop_loss': 232, 'risk_reward': 70, 'chop_rsi_len': 32, 'chop_bandwidth': 229}],
+['j3<I5i', 50, 106, 53.29, 47, 21, 6.18, {'ott_len': 63, 'ott_percent': 197, 'stop_loss': 139, 'risk_reward': 39, 'chop_rsi_len': 14, 'chop_bandwidth': 290}],
+['p2QH5i', 41, 96, 51.95, 44, 18, 2.61, {'ott_len': 69, 'ott_percent': 189, 'stop_loss': 232, 'risk_reward': 38, 'chop_rsi_len': 14, 'chop_bandwidth': 290}]
+]
