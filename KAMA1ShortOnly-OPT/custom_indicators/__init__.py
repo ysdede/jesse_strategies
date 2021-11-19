5c3395895624ecac1c965c9b8c680c78c8e21848
Yunus S. Dede <yunusseyhandede@gmail.com>
2021-11-19 22:37:01 +0300

    Refine

---
+++ KAMA1ShortOnly-OPT/custom_indicators/__init__.py
@@ -0,0 +1,4 @@
+from .ott import ott
+from .var import var
+from .rma import rma
+
---
+++ KAMA1ShortOnly-OPT/routes.py
@@ -0,0 +1,9 @@
+routes = [
+    ('Binance Futures', 'XLM-USDT', '5m', 'Ott2butKAMA1ShortOnly'),
+    ('Binance Futures', 'ETH-USDT', '5m', 'Ott2butKAMA1ShortOnly'),
+    ('Binance Futures', 'BNB-USDT', '5m', 'Ott2butKAMA1ShortOnly'),
+    ('Binance Futures', 'ETC-USDT', '5m', 'Ott2butKAMA1ShortOnly'),
+    ('Binance Futures', 'LINK-USDT', '5m', 'Ott2butKAMA1ShortOnly'),
+]
+
+extra_candles = []
